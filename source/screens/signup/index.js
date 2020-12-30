9487b0a915740bc27807bb24abaa0720c70d6857
Ajay <ajay.pathak@daffodilsw.com>
2020-12-30 19:42:50 +0530

    code optimization in signup file

--- source/screens/signup/index.js
+++ source/screens/signup/index.js
@@ -6,106 +6,73 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
-import {styles} from '../signup/signupstyle'
+import {styles} from '../signup/signupstyle';
 
 const Signup = () => {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [firstnameError, setFirstnameError] = useState('');
-  const [lastnameError, setLastnameError] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [error, setError] = useState({});
+  const [formData, setFormData] = useState({});
   const [isValid, setFormValidity] = useState(true);
-
   const validEmailRegex = RegExp(
     /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
   );
-
   const formField = ['firstname', 'lastname', 'email', 'password'];
-
-  getValue = (fieldName) => {
-    if (fieldName === 'firstname') {
-      return firstname;
-    } else if (fieldName === 'lastname') {
-      return lastname;
-    } else if (fieldName === 'email') {
-      return email;
-    } else if (fieldName === 'password') {
-      return password;
-    } else if (fieldName === 'firstnameError') {
-      return firstnameError;
-    } else if (fieldName === 'lastnameError') {
-      return lastnameError;
-    } else if (fieldName === 'emailError') {
-      return emailError;
-    } else if (fieldName === 'passwordError') {
-      return passwordError;
-    }
-  };
-
-  validateForm = () => {
-    if (
-      firstname.length == 0 ||
-      lastname.length == 0 ||
-      password.length == 0 ||
-      email.length == 0
-    ) {
-      return setFormValidity(true);
-    } else if (
-      firstnameError.length > 0 ||
-      lastnameError.length > 0 ||
-      passwordError.length > 0 ||
-      emailError.length > 0
-    ) {
+  const validateForm = () => {
+    if (Object.keys(formData).length < 4 || Object.keys(error).length) {
       return setFormValidity(true);
     } else {
       return setFormValidity(false);
     }
   };
-  setValue = (text, fieldName) => {
-    switch (fieldName) {
-      case 'email':
-        setEmail(text);
-        validEmailRegex.test(text)
-          ? setEmailError('')
-          : setEmailError('Email is not valid!');
-        break;
-      case 'password':
-        setPassword(text);
-        text.length < 8
-          ? setPasswordError('Password must be 8 characters long!')
-          : setPasswordError('');
-        break;
-      case 'firstname':
-        setFirstname(text);
-        text.length < 2
-          ? setFirstnameError(
-              'first name should be greater then 2 characters long!',
-            )
-          : setFirstnameError('');
-        break;
-      case 'lastname':
-        setLastname(text);
-        text.length < 2
-          ? setLastnameError(
-              'last name should be greater then 2  characters long!',
-            )
-          : setLastnameError('');
-        break;
-      default:
-        break;
+  const setValue = (text, fieldName) => {
+    let formError = error;
+    let Formdata = formData;
+    let fieldError = '';
+    Formdata = {...Formdata, [fieldName]: text};
+    if (text == '') {
+      delete Formdata[fieldName];
+      fieldError = 'Email can not be empty';
+    } else {
+      switch (fieldName) {
+        case 'email':
+          validEmailRegex.test(text)
+            ? (fieldError = '')
+            : (fieldError = 'Email is not valid!');
+          break;
+        case 'password':
+          text.length < 8
+            ? (fieldError = 'Password must be 8 characters long!')
+            : (fieldError = '');
+          break;
+        case 'firstname':
+          text.length < 2
+            ? (fieldError =
+                'first name should be greater then 2 characters long!')
+            : (fieldError = '');
+          break;
+        case 'lastname':
+          text.length < 2
+            ? (fieldError =
+                'last name should be greater then 2  characters long!')
+            : (fieldError = '');
+          break;
+        default:
+          break;
+      }
     }
+    if (fieldError == '') {
+      delete formError[fieldName];
+    } else {
+      formError = {...formError, [fieldName]: fieldError};
+    }
+    setError(formError);
+    setFormData(Formdata);
     validateForm();
   };
-
-  submit = () => {
+  const submit = () => {
     if (!isValid) {
       console.log('Signup successful');
     }
   };
-
   return (
     <ScrollView
       contentContainerStyle={{flexGrow: 1}}
@@ -118,13 +85,11 @@ const Signup = () => {
                 style={styles.textInputStyle}
                 onChangeText={(text) => setValue(text, field)}
                 placeholder={field}
-                value={getValue(field)}
+                value={formData[field]}
                 required
               />
               <View style={styles.errorViewStyle}>
-                <Text style={styles.errorText}>
-                  {getValue(`${field}Error`)}
-                </Text>
+                <Text style={styles.errorText}>{error[`${field}`]}</Text>
               </View>
             </View>
           );
@@ -139,7 +104,4 @@ const Signup = () => {
     </ScrollView>
   );
 };
-
-
-
 export default Signup;
